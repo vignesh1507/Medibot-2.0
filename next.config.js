6b378ef1d504eea10501f41601923943cf80709e
Sujay149 <sujayss149@gmail.com>
2025-07-19 20:27:45 +0530

    domain

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
         pathname: "/dygmrde1v/**",
+        
       },
     ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'medibot-ai',
-          },
-        ],
-        destination: 'medibot-ai-two.vercel.app',
-        permanent: true,
-      },
-    ];
   },
 };
 
