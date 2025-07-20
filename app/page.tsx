cfb4624be69e1072eb4991d09a002efb1fec24db
Sujay Babu Thota <146857894+Sujay149@users.noreply.github.com>
2025-07-20 14:11:42 +0530

    apk integration

--- app/page.tsx
+++ app/page.tsx
@@ -377,7 +377,7 @@ export default function HomePage() {
                 ))}
               </ul>
             <Button asChild className={buttonClasses}>
-  <a href="/mediBot.apk" download="mediBot.apk" target="_blank" rel="noopener noreferrer">
+  <a href="/medibot.apk" download="medibot.apk" target="_blank" rel="noopener noreferrer">
     Download Mobile App
   </a>
 </Button>
