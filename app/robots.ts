6d363e481054942e29e20011e546dcbe86045ce5
Sujay149 <sujayss149@gmail.com>
2025-07-25 15:56:51 +0530

    updated refund

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://medibot.vercel.app'
+  const baseUrl = 'https://medibot-ai.com'
   
   return {
     rules: [
