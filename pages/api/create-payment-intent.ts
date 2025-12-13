7891979e1f669b1681acc426287210b62c7b0377
Vignesh Skanda <agvskanda@gmail.com>
2025-12-13 23:08:01 +0530

    Update create-payment-intent.ts

--- pages/api/create-payment-intent.ts
+++ pages/api/create-payment-intent.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-08-27.basil',
 });
 
 export default async function handler(
